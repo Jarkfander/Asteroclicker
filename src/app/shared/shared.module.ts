8f9b4d92922dbcfe45ce7defd56485c32f1dd566
Florent Dupont <florent.dupont.it@gmail.com>
2017-11-13 15:49:19 +0100

    Added modal for quest + minor fixes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LightboxDirective } from './lightbox.directive';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   exports: [
-    LightboxDirective
+    LightboxDirective,
+    ModalComponent
   ],
-  declarations: [LightboxDirective]
+  declarations: [LightboxDirective, ModalComponent]
 })
 export class SharedModule { }
