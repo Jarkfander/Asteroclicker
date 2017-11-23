0d67e83d49c2481b0fc57dc480c0dd58d536e540
Robin <robinboitemail@gmail.com>
2017-11-23 16:32:46 +0100

    modified : asteroid current capacity

--- src/app/asteroid/asteroid.ts
+++ src/app/asteroid/asteroid.ts
@@ -1,16 +1,18 @@
 export class Asteroid {
 
     public capacity: number;
+    public currentCapacity: number;
     public purity: number;
     public ore: string;
     public timeToGo: number;
     public seed: string;
 
-    constructor(capacity: number, purity: number, ore: string, seed: string, timeToGo: number) {
+    constructor(currentCapacity: number, capacity: number, purity: number, ore: string, seed: string, timeToGo: number) {
+        this.currentCapacity = currentCapacity;
         this.capacity = capacity;
         this.purity = purity;
         this.ore = ore;
-        this.seed= seed;
+        this.seed = seed;
         this.timeToGo = timeToGo;
     }
 }
