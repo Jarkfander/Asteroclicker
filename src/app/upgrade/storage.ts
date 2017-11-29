939fc53e18fcc3666ed0b0e253198ca1c1838c68
Robin <robinboitemail@gmail.com>
2017-11-29 15:25:58 +0100

    Refractored : upgrade

--- src/app/upgrade/storage.ts
+++ src/app/upgrade/storage.ts
@@ -5,7 +5,8 @@ export class Storage extends Upgrade {
     public capacity: number;
 
     constructor(level: number, cost: number, capacity: number, time: number) {
-        super(level, cost, time);
+        super(level, cost, time,'storage','Storage');
+        this.cara["capacity"]=capacity; 
         this.capacity = capacity;
     }
 }
