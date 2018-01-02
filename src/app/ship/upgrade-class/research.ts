b2aa05eeab62e9d238bd3b53b2727a91b17cac42
Niko <niikosaul@gmail.com>
2018-01-02 16:01:10 +0100

    add search

--- src/app/ship/upgrade-class/research.ts
+++ src/app/ship/upgrade-class/research.ts
@@ -7,8 +7,11 @@ export class Research extends Upgrade {
     public maxDistance: number;
     public minDistance: number;
 
-    constructor(level: number, cost: number, time: number, searchTime: number, _maxDistance: number, _minDistance: number) {
+    constructor(level: number, cost: number, time: number, searchTime: number, _maxDistance: number, _minDistance: number, newOreName) {
         super(level, cost, time, 'research', 'Research');
+        if (newOreName) {
+            this.cara['New Ore '] = newOreName;
+        }
         this.cara['searchTime'] = searchTime;
         this.searchTime = searchTime;
         this.maxDistance = _maxDistance;
