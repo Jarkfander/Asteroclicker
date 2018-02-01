ceeb392c2a72801264d83796a58856440537ace1
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2018-02-01 17:51:04 +0100

    sort ore by order and check activated

--- src/app/ore/ore-list/ore-list.component.ts
+++ src/app/ore/ore-list/ore-list.component.ts
@@ -2,23 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import { UserService, IUserUpgrade } from '../../shared/user/user.service';
 import { User } from '../../shared/user/user';
 import { Observable } from 'rxjs/Observable';
-import { OreService, IOreAmounts } from '../ore.service';
 import { ResourcesService } from '../../shared/resources/resources.service';
+import { UpgradeService } from '../../ship/upgrade.service';
+import { OreService, IOreAmounts } from '../ore.service';
+import { staggerTile } from './../../shared/animations';
+
+import { map, take, combineLatest, scan } from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
 
 interface IOreData {
   name: string;
-  amount: number;
+  order: number;
+  amount: Observable<number>;
+  activated: Observable<boolean>;
 }
 
 @Component({
   selector: 'app-ore-list',
   templateUrl: './ore-list.component.html',
-  styleUrls: ['./ore-list.component.scss']
+  styleUrls: ['./ore-list.component.scss'],
+  animations: [staggerTile]
 })
 
 export class OreListComponent implements OnInit {
 
+  private oreInfos: IOreInfos;
   public oreData$: Observable<IOreData[]>;
+  public oreData: IOreData[];
   public nextOre: IOreData;
   public mineRate: number;
   public capacity: number;
@@ -28,10 +38,17 @@ export class OreListComponent implements OnInit {
               private oreInfoS: OreService) {}
 
   ngOnInit() {
-    this.oreData$ = this.oreInfoS.OreAmounts
-      .map((oreAmounts: IOreAmounts) => Object.keys(oreAmounts).map(oreName => {
-        return { name: oreName, amount: oreAmounts[oreName] };
-      }));
+    this.oreInfos = this.oreInfoS.oreInfos;
+    this.oreData = Object.keys(this.oreInfos)
+      .map((name: string) => {
+        return {
+          name: name,
+          amount: this.getAmount(name),
+          order: this.oreInfos[name].order,
+          activated: this.isActivated(name)
+        };
+      })
+      .sort((a: IOreData, b: IOreData) => a.order - b.order);
 
     this.userS.getUpgradeByName('storage')
       .map((upgrade: IUserUpgrade) => this.resourcesS.storage[upgrade.lvl].capacity)
@@ -42,5 +59,19 @@ export class OreListComponent implements OnInit {
       .subscribe((mineRate: number) => this.mineRate = mineRate);
   }
 
+  /** Return the amount of the ore */
+  private getAmount(name: string): Observable<number> {
+    return this.oreInfoS.OreAmounts
+      .map((oreAmounts: IOreAmounts) => oreAmounts[name]);
+  }
+
+  /** Check if ore is activated */
+  public isActivated(name: string): Observable<boolean> {
+    return this.userS.getUpgradeByName('research')
+      .map((upgrade) => upgrade.lvl)
+      .map((researchLvl) => {
+        return this.upgradeS.research[researchLvl].lvl >= this.oreInfos[name].searchNewOre;
+      });
+  }
 
 }
