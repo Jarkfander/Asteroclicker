2a2751db381175b081f2d5d2eb8e8b0da5bd4e7b
RotesMonbin <robinboitemail@gmail.com>
2018-02-22 16:51:16 +0100

    environnement prod fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,22 +1,20 @@
 export const environment = {
-  production: true,
+  production: false,
   firebase: {
-    apiKey: 'AIzaSyBk3U307MoiQsSPAXmzmry7yaAHApU85Rw',
-    authDomain: 'asteroclicker5.firebaseapp.com',
-    databaseURL: 'https://asteroclicker5.firebaseio.com',
-    projectId: 'asteroclicker5',
-    storageBucket: 'asteroclicker5.appspot.com',
-    messagingSenderId: '298728737915'
+    apiKey: "AIzaSyCSzklAwe9ABz2fTpTRseQN-zqIwexbVIQ",
+    authDomain: "asteroclickerush.firebaseapp.com",
+    databaseURL: "https://asteroclickerush.firebaseio.com",
+    projectId: "asteroclickerush",
+    storageBucket: "asteroclickerush.appspot.com",
+    messagingSenderId: "582772943008"
   },
-  socketUrl: 'https://asteroclickerserver.herokuapp.com/',
-  loadingTime: 5,
-  provider: 'https://ropsten.infura.io/Ge8pLCXZNKUB86c7miUf',
-  authClient: 'http://localhost:4201/', // authClient: 'https://b2e-auth-client.herokuapp.com/',
-  clientId: 'urn:b2e:nxcrus',
-
-  // Testing addresses to be changed
+  socketUrl: 'http://localhost:4000',
+  loadingTime: 0,
   addresses: {
     nexium: '0xab904d9c85388653e6fb1c75b41fbc0465d39b90',
     boostMarket: '0x1129c0721a4200b3d0839e2a6079992e0b685959'
-  }
+  },
+  provider: 'https://ropsten.infura.io/Ge8pLCXZNKUB86c7miUf',
+  authClient: 'http://localhost:4201/',
+  clientId: 'urn:b2e:nxcrus'
 };
